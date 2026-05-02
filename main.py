fdf8d7c49ed2c9c8e5dc959dfc2dfc3e94de52cc
vidpod <vid.podrekar@student.um.si>
2026-05-02 23:40:43 +0200

    Added error log

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ def add(a, b):
 
 
 def main():
-
+    try:
         a = float(input("Vnesi prvo število (a): "))
         b = float(input("Vnesi drugo število (b): "))
 
         result = add(a, b)
         print(result)
+
+    except ValueError:
+        print("Napaka: vnesi števili!")
 
 
 if __name__ == "__main__":
